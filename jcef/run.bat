7aaf8cfbe02a2397ebb43cb6ae5dd3e8136fdd88
sohamkale <sohamkale@ufl.edu>
2020-10-11 20:22:39 -0400

    not working!
    removed libraries locally

--- jcef/run.bat
+++
@@ -1,5 +0,0 @@
-:: Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
-:: reserved. Use of this source code is governed by a BSD-style license
-:: that can be found in the LICENSE file.
-
-java -cp "./;./*" -Djava.library.path=./lib/win64 tests.detailed.MainFrame
